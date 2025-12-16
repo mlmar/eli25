f8c0290c31b9641306a7b98854e98936e7f8374b
mlmar <marcloussm@gmail.com>
2025-12-16 15:10:24 -0800

    Moving tokenizer and model outside of a function

--- src/server/lib/hugging_face.py
+++ src/server/lib/hugging_face.py
@@ -2,24 +2,15 @@ from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
 
 import config
 
-__pipe = None
-def get_pipeline():
-    """Initializes pipeline model"""
-    global __pipe
-    if __pipe is None and config.MODEL:
-        model_id = config.MODEL
-        print(f'Using model ${model_id}')
-        tokenizer = AutoTokenizer.from_pretrained(model_id)
-        model = AutoModelForSeq2SeqLM.from_pretrained(
-            model_id,
-            device_map='auto'
-        )
-        __pipe = pipeline("text2text-generation", model=model, tokenizer=tokenizer)
-
-    return __pipe
+model_id = config.MODEL
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForSeq2SeqLM.from_pretrained(
+    model_id,
+    device_map='auto'
+)
+pipe = pipeline("text2text-generation", model=model, tokenizer=tokenizer)
 
 def generate_text(prompt: str) -> str:
-    pipe = get_pipeline()
     summary = pipe(prompt)
     return summary[-1]['generated_text']
 
