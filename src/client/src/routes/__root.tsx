5a8be0b5b055404f56f4dccd8a705da9dc1e295f
mlmar <marcloussm@gmail.com>
2025-12-23 09:16:21 -0800

    Added header click to return to current date

--- src/client/src/routes/__root.tsx
+++ src/client/src/routes/__root.tsx
@@ -1,4 +1,4 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router';
+import { Link, Outlet, createRootRoute } from '@tanstack/react-router';
 import { NewsPage } from '@/features/NewsPage';
 import { IconLink } from '@/components/IconLink';
 import { css } from '@/util/css';
@@ -11,23 +11,23 @@ export const Route = createRootRoute({
 
 function RootComponent() {
     return (
-        <>
-            <main className='bg-neutral-5500 h-full w-full flex flex-col flex-auto justify-center bg-neutral-200'>
-                <header
-                    className={css(
-                        'flex items-center justify-between lg:pl-10 lg:pr-10 pl-5 pr-5 pt-3 pb-3 text-white shadow-md',
-                        styles.darkBg
-                    )}
-                >
+        <main className='bg-neutral-5500 h-full w-full flex flex-col flex-auto justify-center bg-neutral-200'>
+            <header
+                className={css(
+                    'flex items-center justify-between lg:pl-10 lg:pr-10 pl-5 pr-5 pt-3 pb-3 text-white shadow-md',
+                    styles.darkBg
+                )}
+            >
+                <Link to='..'>
                     <h1 className='font-bold text-xl'> eli25 </h1>
-                    <span className='flex gap-5'>
-                        <IconLink className='invert h-7 w-7' src={githubImage} url={githubUrl} />
-                        <IconLink className='h-full h-7 w-7' src={portfolioImage} url={portfolioUrl} />
-                    </span>
-                </header>
-                <Outlet />
-            </main>
-        </>
+                </Link>
+                <span className='flex gap-5'>
+                    <IconLink className='invert h-7 w-7' src={githubImage} url={githubUrl} />
+                    <IconLink className='h-full h-7 w-7' src={portfolioImage} url={portfolioUrl} />
+                </span>
+            </header>
+            <Outlet />
+        </main>
     );
 }
 
--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -10,6 +10,9 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     server: {
         port: 3000,
+        watch: {
+            ignored: ['./src/routeTree.gen.ts']
+        }
     },
     resolve: {
         alias: {
