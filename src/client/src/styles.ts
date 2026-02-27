45864a0abf95530bee3342d69cca5a00ca27241e
mlmar <marcloussm@gmail.com>
2026-02-27 01:49:45 -0800

    Updating styles, removing margins

--- src/client/src/styles.ts
+++ src/client/src/styles.ts
@@ -5,6 +5,6 @@ export const styles = {
     cardRadius: 'rounded-sm',
     altBg: 'bg-indigo-600',
     altTextColor: 'text-indigo-600',
-    navigation: 'flex lg:mx-10 lg:justify-start md:mx-5 justify-between xs:justify-center text-2xl font-bold gap-1 ',
+    navigation: 'flex lg:justify-start justify-between xs:justify-center text-2xl font-bold gap-1 ',
     h3: 'lg:mt-10 lg:ml-10 lg:mr-10 m-5 mb-0 text-2xl font-bold'
 }
